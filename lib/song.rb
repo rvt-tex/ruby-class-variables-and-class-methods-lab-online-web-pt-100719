da63c36d37df51561dd6d77b5af0b8fbe25a63f5
Ruel Tingling <rvtingling@gmail.com>
2019-10-28 22:06:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,52 +1,46 @@
 class Song
-  attr_accessor :name, :artist, :genre 
-    @@count = 0 
-    @@artist = []
-    @@genre = []
-  
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre 
-    @@count += 1 
-    @@artist << artist
-    @@genre << genre
-  end 
-
-  def self.count
-    @@count 
-  end 
-  
-  def self.artist
-    @@artist.uniq
-  end
-  
-  def self.genre
-    @@genre.uniq
-  end 
-
-  def self.artist_count
-   artist_count = {}
- @@artist.each{ |artist|
-    if artist_count[artist]
-      artist_count[artist] += 1
-   else
-     artist_count[artist] = 1
-   end
-  }
-  artist_count
+ @@count = 0
+ @@artists = []
+ @@genres = []
+ attr_accessor :name, :artist, :genre
+ def initialize(name, artist, genre)
+   @name = name
+   @artist = artist
+   @genre = genre
+   @@count += 1
+   @@artists << artist
+   @@genres << genre
  end
-
-  def self.genre_count
+ def self.count
+   @@count
+ end
+ def self.artists
+   @@artists.uniq
+ end
+ def self.genres
+   @@genres.uniq
+ end
+ def self.genre_count
    genre_count = {}
- @@genre.each{ |genre|
-    if genre_count[genre]
+   @@genres.each{ |genre|
+   if genre_count[genre]
      genre_count[genre] += 1
    else
      genre_count[genre] = 1
    end
-  }
-  genre_count
+   }
+   genre_count
  end
-  
+ def self.artist_count
+   artist_count = {}
+   @@artists.each{ |artist|
+     if artist_count[artist]
+       artist_count[artist] += 1
+     else
+       artist_count[artist] = 1
+     end
+   }
+   artist_count
+ end
+end
   
